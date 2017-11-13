bf46483986e2956a4ec105a5876c7662ffc38f1f
Michael <mveronie@gmail.com>
2017-11-13 02:17:53 -0500

    Hid API keys

--- weather/weather.js
+++ weather/weather.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
+const keys = require('./keys/keys');
+
 let getWeather = (lat, lng, callback) => {
   request({
-    url: `https://api.darksky.net/forecast/4998aa7c9fec16a0c1c66d296c77ca0c/${lat},${lng}`,
+    url: `https://api.darksky.net/forecast/${keys.weatherKey}/${lat},${lng}`,
     json: true
   }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
